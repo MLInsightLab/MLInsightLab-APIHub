533dcdfdfc19d6037d3fbe968ffb5e5a0a6f324c
Jacob Renn <jacob.renn4@gmail.com>
2025-01-14 15:49:11 -0500

    updating default image name
    Signed-off-by: Jacob Renn <jacob.renn4@gmail.com>

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ setup_database()
 
 # Instantiate the model manager
 manager = ModelManager(
-    model_image='ghcr.io/mlinsighttechnologies/mlinsightlab-model-container:main',
+    model_image='ghcr.io/mlinsightlab/mlinsightlab-model-container:main',
     model_network=os.getenv('MODEL_NETWORK') if os.getenv('MODEL_NETWORK') else 'mlinsightlab_model_network',
     mlflow_tracking_uri=MLFLOW_TRACKING_URI,
     model_port='8888'
